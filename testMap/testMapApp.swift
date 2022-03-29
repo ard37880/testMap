8772291f93e72aec2e73fb68321886f266150c78
Andrew Davis <andrew.davis37880@gmail.com>
2022-03-29 15:00:40 -0500

    Initial Commit

---
+++ testMap/testMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  testMapApp.swift
+//  testMap
+//
+//  Created by Andrew Davis on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct testMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
