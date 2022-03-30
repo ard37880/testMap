f81ed79c5319089e4a4272affd3d6b9f79a8396d
Andrew Davis <andrew.davis37880@gmail.com>
2022-03-30 14:46:09 -0500

    Working build to website, except for bars on top and bottom on log in and it's still on demo; However, this is proof full site works

--- testMap/ContentView.swift
+++ testMap/ContentView.swift
@@ -4,20 +4,23 @@
 //
 //  Created by Andrew Davis on 3/29/22.
 //
+//  admin 199eRTMx%
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            SwiftUIWebView (url: URL(string: "https://www.rastrac.net"))
-                .navigationTitle("a")
+            SwiftUIWebView (url: URL(string: "https://demo.rastrac.net/rtds?package=full"))
+                .navigationTitle("")
                 .edgesIgnoringSafeArea(.all)
                 .navigationBarHidden(true)
         }
         
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
