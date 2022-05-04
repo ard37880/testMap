086021a2b2a742b1560bf864d1fd44b2901d6de9
Proto <34947107+ard37880@users.noreply.github.com>
2022-05-04 10:24:51 -0500

    Update ContentView.swift

--- testMap/ContentView.swift
+++ testMap/ContentView.swift
@@ -4,14 +4,13 @@
 //
 //  Created by Andrew Davis on 3/29/22.
 //
-//  admin 199eRTMx%
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         NavigationView {
-            SwiftUIWebView (url: URL(string: "https://demo.rastrac.net/rtds?package=full"))
+            SwiftUIWebView (url: URL(string: /*"URL HERE"*/))
                 .navigationTitle("")
                 .edgesIgnoringSafeArea(.all)
                 .navigationBarHidden(true)
